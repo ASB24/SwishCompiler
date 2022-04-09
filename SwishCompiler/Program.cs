d445a72a2c184b8e6f1d76a29b8fee01347c57be
Alex <alexserafini2001@gmail.com>
2022-04-09 00:11:33 -0400

    List of Sets
    For the interpretations of each line

--- SwishCompiler/Program.cs
+++ SwishCompiler/Program.cs
@@ -10,7 +10,25 @@ namespace SwishCompiler
     {
         static void Main(string[] args)
         {
-           
+
+            List<HashSet<string>> lines = new List<HashSet<string>>();
+            string line = " ";
+
+            HashSet<string> temp = new HashSet<string>();
+
+            while (true)
+            {
+                line = Console.ReadLine();
+
+                if (line != null || line == "EOF") break;
+
+                foreach(string variable in line.Split(' '))
+                {
+                    temp.Add(variable);
+                }
+                lines.Add(temp);
+                temp.Clear();
+            }
         }
     }
 }
